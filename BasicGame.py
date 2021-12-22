4c22381e5e7fe1e696b250590e42c47f0a34be3c
VictorBjerrum <96462208+VictorBjerrum@users.noreply.github.com>
2021-12-22 15:35:37 +0100

    Added point system

--- BasicGame.py
+++ BasicGame.py
@@ -6,4 +6,16 @@ RuderPoints = []
 SparPoints = []
 KloverPoints = []
 
+RandomNumber = [randint(1,52)]
 
+for number in RandomNumber:
+    if 39 <= number <= 52:
+        HjertePoints.append("X")
+    elif 26 <= number <= 38:
+         RuderPoints.append("X")
+    elif 13 <= number <= 25:
+        SparPoints.append("X")
+    else:
+        KloverPoints.append("X")
+        
+print(HjertePoints, RuderPoints, SparPoints, KloverPoints)
